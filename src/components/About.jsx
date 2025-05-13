a0a052f823f8a9a28be2bb76fb1aa3e6b0a512f0
moshiurrashid <moshiurrashiduddag@gmail.com>
2025-05-13 18:40:57 +0600

    Homework Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <section className='lg:pt-56 lg:pb-32'>
-        <div className="container">
-            <div className=' block lg:flex justify-between items-center'>
-
-                <div className="aboutTitle text-left lg:w-2/5 relative mb-5 lg:mb-0">
-                    <div className='hidden lg:block absolute -top-40'>
-                        <img src="images/aboutOne.png" alt="vector" />
-                    </div>
-                    <h2 className='heading'>About <span className='text-secondery'>me</span></h2>
-                    <div className='hidden xl:block absolute right-16 -top-5'>
-                        <img src="images/aboutTwo.png" alt="vector" />
-                    </div>
-                    <p className='para'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.... <span className='font-bold'>Read more</span></p>
-                    <div className='hidden lg:block absolute -bottom-50 left-40'>
-                        <img src="images/aboutThree.png" alt="vector" />
-                    </div>
-                </div>
-
-                <div className="aboutImage">
-                    <img className='mx-auto' src="images/aboutImage.png" alt="image" />
-                </div>
+    <section className="lg:pt-56 lg:pb-32">
+      <div className="container">
+        <div className="block items-center justify-between lg:flex">
+          <div className="aboutTitle relative mb-5 text-left lg:mb-0 lg:w-2/5">
+            <div className="absolute -top-40 hidden lg:block">
+              <img src="images/aboutOne.png" alt="vector" />
             </div>
+            <h2 className="heading">
+              About <span className="text-secondery">me</span>
+            </h2>
+            <div className="absolute -top-5 right-16 hidden xl:block">
+              <img src="images/aboutTwo.png" alt="vector" />
+            </div>
+            <p className="para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et velit interdum, ac aliquet odio mattis. Class
+              aptent taciti sociosqu ad litora torquent per conubia nostra, per
+              inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+              lobortis.... <span className="font-bold">Read more</span>
+            </p>
+            <div className="absolute -bottom-50 left-40 hidden lg:block">
+              <img src="images/aboutThree.png" alt="vector" />
+            </div>
+          </div>
 
+          <div className="aboutImage">
+            <img className="mx-auto" src="images/aboutImage.png" alt="image" />
+          </div>
         </div>
-        <hr className='text-white/10 mt-[70px]' />
+      </div>
+      <hr className="mt-[70px] text-white/10" />
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-import Banner from "../Banner";
-import { useState } from "react";
-import { getDatabase, ref, set, push } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { MdErrorOutline } from "react-icons/md";
+import {
+  getDatabase,
+  push,
+  ref,
+  set,
+  onValue,
+  remove,
+} from "firebase/database";
+import { useNavigate } from "react-router";
 
-const Home = () => {
-  const [data, setData] = useState("");
-  const [data2, setData2] = useState("");
+const App = () => {
+  const [task, setTask] = useState("");
+  const [taskError, setTaskError] = useState("");
+  const [tasklist, setTasklist] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const db = getDatabase();
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setData(e.target.value);
+  const handleTask = (e) => {
+    setTask(e.target.value);
   };
+  // Create Data
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!data) {
-      setData2("Task is Required");
+    if (!task) {
+      setTaskError("Task is Required");
     } else {
-      setData2("");
+      setTaskError("");
       const db = getDatabase();
       set(push(ref(db, "todolist/")), {
-        name: data,
+        name: task,
       })
         .then(() => {
+          setTask("");
           console.log("Data send successfull");
         })
         .catch((err) => {
@@ -28,46 +41,147 @@ const Home = () => {
         });
     }
   };
-  console.log(data);
+
+  // Read Data
+  function Todolist() {
+    const todolistRef = ref(db, "todolist/");
+    onValue(todolistRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        arr.push({ ...item.val(), id: item.key });
+        setLoading(false);
+      });
+      setTasklist(arr);
+    });
+  }
+
+  useEffect(() => {
+    Todolist();
+  }, []);
+
+  const handleDelete = (id) => {
+    remove(ref(db, "todolist/" + id));
+    console.log("deleted", id);
+  };
+
+  const handleEdit = (id) => {
+    navigate(`/edit/${id}`);
+  };
 
   return (
-    <div>
-      <section className="bg-white dark:bg-gray-900">
+    <>
+      <section className="h-screen">
         <div className="mx-auto max-w-2xl px-4 py-8 lg:py-16">
           <form action="#">
             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-              <div className="sm:col-span-2">
+              <div className="relative sm:col-span-2">
                 <label
                   htmlFor="name"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                  className="mb-2 block text-sm font-medium text-white"
                 >
-                  Product Name
+                  Enter Your Task
                 </label>
                 <input
-                  onChange={handleChange}
+                  onChange={handleTask}
                   type="text"
                   name="name"
                   id="name"
-                  className="focus:ring-primary-600 focus:border-primary-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
-                  placeholder="Type product name"
+                  value={task}
+                  className={`focus:ring-primary-600 focus:border-primary-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border ${
+                    taskError ? "border-red-500" : "border-gray-300"
+                  } bg-gray-50 p-2.5 text-sm text-gray-900`}
+                  placeholder="Enter Your Task"
                   required=""
                 />
-                {setData2 && <p className="text-red-500">{data2}</p>}
+                {taskError && (
+                  <MdErrorOutline className="absolute top-1/2 right-4 text-2xl text-red-500" />
+                )}
               </div>
-
-              <button
-                onClick={handleSubmit}
-                type="submit"
-                className="hover:bg-primary-800 text-gray inline-flex items-center rounded-lg bg-white px-5 py-2.5 text-center text-sm font-medium focus:ring-1"
-              >
-                Add product
-              </button>
             </div>
+            <button
+              onClick={handleSubmit}
+              type="submit"
+              className="mt-4 items-center rounded-lg border border-gray-300 bg-gray-50 px-5 py-2.5 text-center text-sm font-medium text-gray-900"
+            >
+              Add Task
+            </button>
           </form>
+
+          <div className="mx-auto mt-5 max-w-2xl rounded-xl border border-gray-300 bg-gray-50 p-5">
+            {loading ? (
+              <div
+                role="status"
+                className="animate-pulse rounded-sm p-4 shadow-sm"
+              >
+                <div className="flex items-center justify-between">
+                  <div>
+                    <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-200" />
+                    <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-200" />
+                  </div>
+                  <div className="h-2.5 w-12 rounded-full bg-gray-200 dark:bg-gray-200" />
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                  <div>
+                    <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-200" />
+                    <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-200" />
+                  </div>
+                  <div className="h-2.5 w-12 rounded-full bg-gray-200 dark:bg-gray-200" />
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                  <div>
+                    <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-200" />
+                    <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-200" />
+                  </div>
+                  <div className="h-2.5 w-12 rounded-full bg-gray-200 dark:bg-gray-200" />
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                  <div>
+                    <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-200" />
+                    <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-200" />
+                  </div>
+                  <div className="h-2.5 w-12 rounded-full bg-gray-200 dark:bg-gray-200" />
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                  <div>
+                    <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-200" />
+                    <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-200" />
+                  </div>
+                  <div className="h-2.5 w-12 rounded-full bg-gray-200 dark:bg-gray-200" />
+                </div>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : (
+              <ul>
+                {tasklist.map((item, i) => (
+                  <li className="flex items-center justify-between p-2 font-medium">
+                    <span className="w-1/2">
+                      {i + 1}. {item.name}{" "}
+                    </span>
+                    <div>
+                      <button
+                        onClick={() => handleDelete(item.id)}
+                        type="button"
+                        className="pr- mr-4 rounded-lg bg-red-600 px-5 py-2.5 text-sm font-medium text-white"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => handleEdit(item.id)}
+                        type="button"
+                        className="rounded-lg bg-green-500 px-5 py-2.5 text-sm font-medium text-white"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </section>
-    </div>
+    </>
   );
 };
 
-export default Home;
+export default App;
